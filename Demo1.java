3358390163df160fa87393939a136d1a1e7ebee7
SagarBirajdar <birajdarsager1010@gmail.com>
2023-04-24 12:54:33 +0530

    Changes done completely

--- Demo1.java
+++ Demo1.java
@@ -1,7 +1,10 @@
 public class Student 
 {
 
+public void main(String args) {
+
 System.out.println("Hello sagar");
 
+}
 
 }
---
+++ Demo2.java
@@ -0,0 +1,9 @@
+public Class Demo02
+	{
+
+public void main(String[] args) {
+
+System.out.println("This is second class");
+
+}
+}
